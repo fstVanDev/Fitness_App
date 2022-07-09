74bd11077a2f41d0b89453ad2b3ea122fef8fa8e
Khandoga Kirill <fstDev.git@gmail.com>
2022-07-09 20:52:13 +0600

    working app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,21 @@
-import React, { useState } from 'react'
-import { Box } from '@mui/material'
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
 
-import Exercises from '../components/Exercises'
-import HeroBanner from '../components/HeroBanner'
-import SearchExercises from '../components/SearchExercises'
- 
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
 
 const Home = () => {
-  return (
-     <Box>
-        <HeroBanner />
-        <SearchExercises />
-        <Exercises/>
-    </Box>
-  )
-}
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
 
-export default Home
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
+    </Box>
+  );
+};
+
+export default Home;
